1ba709f5dcd4a980ec19b77f12cab9b502ff2069
Marco A. Lopez <ma.lopezdelanieta@gmail.com>
2016-02-28 18:59:16 +0100

    Added Item class

--- gom_jabbar/cart.rb
+++ gom_jabbar/cart.rb
@@ -1,3 +1,14 @@
+# Clase Item
+# ==========
+class Item
+  # No incluyo el nombre porque no es necesario para hacer la lógica
+  attr_accessor :code, :price
+  def initialize(code,price)
+    @code = code
+    @price = price
+  end
+end
+
 class Cart
   def initialize(pricing_rules = nil)
   end
