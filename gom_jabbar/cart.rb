7702df9a50121ecee10e68b944dc19092ff5598a
Marco A. Lopez <ma.lopezdelanieta@gmail.com>
2016-02-28 19:02:57 +0100

    Added Discount classes

--- gom_jabbar/cart.rb
+++ gom_jabbar/cart.rb
@@ -9,6 +9,38 @@ class Item
   end
 end
 
+# Clases para los descuentos
+# ==========================
+# Clase para obtener el precio con el descuento 2x1
+class DiscountTwoForOne
+  # Se divide entre 2 y se suma 1 si es impar
+  def total(quantity, price)
+    (quantity % 2 + (quantity / 2)) * price
+  end
+end
+# Clase para obtener el precio con el descuento 3+
+class DiscountThreeOrMore
+  # Inicializo porque esta promo tiene un nuevo precio
+  def initialize(new_price)
+    @new_price = new_price
+  end
+  # Si el numero de items es >= 3 entonces se aplica el nuevo precio al item
+  def total(quantity, price)
+    if quantity >= 3 then
+      price = @new_price
+    end
+    quantity * price
+  end
+end
+# Clase para obtener el precio sin aplicar descuento
+class DiscountNull
+  # Si no hay descuento el precio no se modifica
+  # y el total es igual al precio del item por la cantidad de items
+  def total(quantity,price)
+    quantity * price
+  end
+end
+
 class Cart
   def initialize(pricing_rules = nil)
   end
